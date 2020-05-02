33dcc3a2da05af4f29e8695e793c06fa391ead39
Vinh Mao <vinhxmao87@gmail.com>
2020-05-02 15:50:22 -0700

    markdown code mostly generated, filling in user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,52 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
+[![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)]
+(https://github.com/johnfyoung/google)
 
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} license.
+  
+## Contributing
+
+${data.contributions}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact [${data.name}](undefined) directly at john@codeandcreative.com.
 `;
 }
 
